13884ccfbc3838417bff94c604a0bff5b91cc8d1
Michael <mcbarre12@gmail.com>
2024-03-20 15:28:34 -0500

    initial commit

---
+++ src/Kyle.tsx
@@ -0,0 +1,24 @@
+
+import { Intent, Spinner, Button, OverlaysProvider, Dialog } from "@blueprintjs/core"
+import { useState } from "react";
+
+
+function Kyle() {
+
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <OverlaysProvider>
+    <div>
+    <h1>Building Website</h1>
+    <Spinner intent={Intent.DANGER} size={400}/>
+    <br/>
+    <Button text="Where Website?" onClick={() => setOpen(true)} large={true}/>
+    <Dialog isOpen={isOpen} onClose={() => setOpen(false)}>
+      <h1>NOT BUILD YET!</h1>
+    </Dialog>
+    </div>
+    </OverlaysProvider>
+  )
+}
+
+export default Kyle
